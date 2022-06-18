46d7d70310c90a2413b437f8bdfa29ce5b9cc818
igoraugustobrz <igoraugustobz@gmail.com>
2022-06-17 21:55:48 -0300

    Javascript Events

---
+++ Javascript/js/click.js
@@ -0,0 +1,9 @@
+function umClique(){
+    document.getElementById("imagem").src="img/fachada.jpg";
+    document.getElementById("log").innerHTML += "troca para a foto da fachada<br>";
+}
+
+function doisCliques(){
+    document.getElementById("imagem").src="img/ifrs.gif";
+    document.getElementById("log").innerHTML += "troca para o logo do IFRS-BG<br>";
+}
